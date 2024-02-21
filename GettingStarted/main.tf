56213cbe87e029131f5376efa22c749764470575
unknown <juanjosesierram@gmail.com>
2024-02-21 15:51:56 -0500

    Feat: Using variables in terraform

--- GettingStarted/main.tf
+++ GettingStarted/main.tf
@@ -4,14 +4,14 @@
 
 //Terraform destroy deletes the file
 resource "local_file" "pet" {
-    content = "We love pets!"
-    filename = "./pets.txt"
+    content = var.content
+    filename = var.filename
     file_permission = "0700"
 }
 
 //Outputs a random pet name
 resource "random_pet" "my-pet" {
-    prefix = "Mrs"
-    separator = "."
-    length = "1"
+    prefix = var.prefix
+    separator = var.separator
+    length = var.length
 }
