dc2586c2643a3ab2d0523d5910578c5410bd64b6
unknown <juanjosesierram@gmail.com>
2024-02-21 17:11:50 -0500

    Feat(main.tf) Resource attributes

--- GettingStarted/main.tf
+++ GettingStarted/main.tf
@@ -4,14 +4,14 @@
 
 //Terraform destroy deletes the file
 resource "local_file" "pet" {
-    content = var.file-content["statement2"]
+    content = "My favorite pet is ${random_pet.my-pet.id}"
     filename = var.filename
     file_permission = "0700"
 }
 
 //Outputs a random pet name
 resource "random_pet" "my-pet" {
-    prefix = var.prefix[0]
+    prefix = "Mr"
     separator = var.separator
     length = var.length
 }
