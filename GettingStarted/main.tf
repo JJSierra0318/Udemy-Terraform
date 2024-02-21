548155c3a8b4c94e79453fef2ac2b6ed1bee7f26
unknown <juanjosesierram@gmail.com>
2024-02-21 16:00:08 -0500

    Feat(variables.tf): Variables types and uses

--- GettingStarted/main.tf
+++ GettingStarted/main.tf
@@ -4,14 +4,14 @@
 
 //Terraform destroy deletes the file
 resource "local_file" "pet" {
-    content = var.content
+    content = var.file-content["statement2"]
     filename = var.filename
     file_permission = "0700"
 }
 
 //Outputs a random pet name
 resource "random_pet" "my-pet" {
-    prefix = var.prefix
+    prefix = var.prefix[0]
     separator = var.separator
     length = var.length
 }
--- GettingStarted/variables.tf
+++ GettingStarted/variables.tf
@@ -1,15 +1,45 @@
 variable "filename" {
+    type = string
     default = "./pets.txt"
 }
-variable "content" {
-    default = "We love pets!"
-}
-variable "prefix" {
-    default = "Mrs"
-}
 variable "separator" {
+    type = string
     default = "."
 }
 variable "length" {
-    default = "1"
+    type = number
+    default = 1
+}
+//type list is also available
+variable "prefix" {
+    type = set(string)
+    default = ["Mr", "Mrs", "Sir"]
+}
+variable "file-content" {
+    type = map
+    default = {
+        "statement1" = "We love pets!"
+        "statement2" = "We love animals!"
+    }
+}
+variable "bella" {
+    type = object({
+      name = string
+      color = string
+      age = number
+      food = list(string)
+      favorite_pet = bool
+    })
+
+    default = {
+      name = "Bella"
+      color = "brown"
+      age = 7
+      food = [ "fish", "chicken" ]
+      favorite_pet = true
+    }
+}
+variable "kitty" {
+    type = tuple([ string, number, bool ])
+    default = [ "cat", 7, false ]
 }
