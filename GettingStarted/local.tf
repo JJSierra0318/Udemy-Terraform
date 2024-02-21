19e59539c3a979496e3ac171ee1756037a2c29d0
unknown <juanjosesierram@gmail.com>
2024-02-21 14:27:29 -0500

    Feat(local.tf) Update and destroy resource

--- GettingStarted/local.tf
+++ GettingStarted/local.tf
@@ -1,7 +1,10 @@
 //resource "from_to" "resourceName" {
 //  parameters = "values"
 //}
+
+//Terraform destroy deletes the file
 resource "local_file" "pet" {
     content = "We love pets!"
     filename = "./pets.txt"
+    file_permission = "0700"
 }
