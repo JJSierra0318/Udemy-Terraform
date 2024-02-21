de46a8f40618be811834250210e5b93fdd0903ea
unknown <juanjosesierram@gmail.com>
2024-02-21 17:29:02 -0500

    Feat(main.tf): outputs

--- GettingStarted/main.tf
+++ GettingStarted/main.tf
@@ -7,6 +7,8 @@ resource "local_file" "pet" {
     content = "My favorite pet is ${random_pet.my-pet.id}"
     filename = var.filename
     file_permission = "0700"
+    //Dependency if the reference expression isn't used
+    //depends_on = [ random_pet.my-pet ]
 }
 
 //Outputs a random pet name
@@ -14,4 +16,9 @@ resource "random_pet" "my-pet" {
     prefix = "Mr"
     separator = var.separator
     length = var.length
+}
+
+//Can use terraform output
+output "pet-name" {
+    value = random_pet.my-pet.id
 }
