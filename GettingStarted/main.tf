54bfb6d29ef0303b799e797cf62798cb90fb9fcc
Juan Jose Sierra Mejia <juanjose.sierra@rockwellautomation.com>
2024-02-22 09:53:22 -0500

    Feat: Terraform data sources

--- GettingStarted/main.tf
+++ GettingStarted/main.tf
@@ -4,7 +4,7 @@
 
 //Terraform destroy deletes the file
 resource "local_file" "pet" {
-    content = "My favorite pet is ${random_pet.my-pet.id}"
+    content = "My favorite pet is ${random_pet.my-pet.id}\nAlso ${data.local_file.dog.content}"
     filename = var.filename
     file_permission = "0700"
     //Dependency if the reference expression isn't used
@@ -24,6 +24,10 @@ resource "random_pet" "my-pet" {
     prefix = "Mr"
     separator = var.separator
     length = var.length
+}
+
+data "local_file" "dog" {
+    filename = "./dog.txt"
 }
 
 //Can use terraform output
